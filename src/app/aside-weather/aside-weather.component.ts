a2387618fa85ffe3e549d82af99fa688b7bf6452
Marcos Lopez <markosstone@gmail.com>
2024-01-31 22:51:32 +0100

    Primer commit

---
+++ src/app/aside-weather/aside-weather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aside-weather',
+  standalone: true,
+  imports: [],
+  templateUrl: './aside-weather.component.html',
+  styleUrl: './aside-weather.component.css'
+})
+export class AsideWeatherComponent {
+
+}
